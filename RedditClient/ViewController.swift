b5eda5902717de83d5bfcde706fde03e1e3bf3d0
Yaroslav <q.link0.p@gmail.com>
2020-10-28 20:35:52 +0200

    Initial Commit

---
+++ RedditClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditClient
+//
+//  Created by Yaroslav on 28.10.2020.
+//  Copyright © 2020 Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
